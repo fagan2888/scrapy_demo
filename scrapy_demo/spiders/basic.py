307dac7e6c774391e16f73335da60dcc04cc8e35
kakuilan <kakuilan@163.com>
2019-10-26 13:35:22 +0800

    scrapy crawl basic

--- scrapy_demo/spiders/basic.py
+++ scrapy_demo/spiders/basic.py
@@ -5,7 +5,14 @@ import scrapy
 class BasicSpider(scrapy.Spider):
     name = 'basic'
     allowed_domains = ['quotes.toscrape.com']
-    start_urls = ['http://quotes.toscrape.com/']
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
 
+    #执行爬取 scrapy crawl basic
     def parse(self, response):
-        pass
+        page = response.url.split("/")[-2]
+        filename = 'toscrape-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
