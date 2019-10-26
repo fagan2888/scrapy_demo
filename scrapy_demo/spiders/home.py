347c0066c8c703c8fb70f714fa8f946dfe6e8a42
kakuilan <kakuilan@163.com>
2019-10-26 16:12:41 +0800

    fix 使用item装载器ItemLoader

--- scrapy_demo/spiders/home.py
+++ scrapy_demo/spiders/home.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy_demo.items import ScrapyDemoItem
+from scrapy.loader import ItemLoader
 
 
 class HomeSpider(scrapy.Spider):
@@ -16,11 +17,20 @@ class HomeSpider(scrapy.Spider):
         # self.log('image_urls: %s' % response.xpath('//*[@itemprop="image"][1]/@src').extract())
 
         # 创建一个新的item
-        item = ScrapyDemoItem()
-        item['title'] = response.xpath('//*[@itemprop="name"][1]/text()').extract()
-        item['price'] = response.xpath('//*[@itemprop="price"][1]/text()').re('[.0-9]+')
-        item['description'] = response.xpath('//*[@itemprop="description"][1]/text()').extract()
-        item['address'] = response.xpath('//*[@itemtype="http://schema.org/Place"][1]/text()').extract()
-        item['images'] = response.xpath('//*[@itemprop="image"][1]/@src').extract()
-        # 使用yield返回,而非return item
-        yield item
+        # item = ScrapyDemoItem()
+        # item['title'] = response.xpath('//*[@itemprop="name"][1]/text()').extract()
+        # item['price'] = response.xpath('//*[@itemprop="price"][1]/text()').re('[.0-9]+')
+        # item['description'] = response.xpath('//*[@itemprop="description"][1]/text()').extract()
+        # item['address'] = response.xpath('//*[@itemtype="http://schema.org/Place"][1]/text()').extract()
+        # item['images'] = response.xpath('//*[@itemprop="image"][1]/@src').extract()
+        # # 使用yield返回,而非return item
+        # yield item
+
+        # 使用item装载器ItemLoader
+        ld = ItemLoader(item=ScrapyDemoItem(), response=response)
+        ld.add_xpath('title', '//*[@itemprop="name"][1]/text()')
+        ld.add_xpath('price', '//*[@itemprop="price"][1]/text()', re='[,.0-9]+')
+        ld.add_xpath('description', '//*[@itemprop="description"][1]/text()')
+        ld.add_xpath('address', '//*[@itemtype="http://schema.org/Place"][1]/text()')
+        ld.add_xpath('images', '//*[@itemprop="image"][1]/@src')
+        yield ld.load_item()
