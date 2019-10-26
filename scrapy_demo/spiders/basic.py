985706f9d65fd5d71aa93f6a100dee522e163eb4
kakuilan <kakuilan@163.com>
2019-10-26 13:59:19 +0800

    fix 提取并保存数据

--- scrapy_demo/spiders/basic.py
+++ scrapy_demo/spiders/basic.py
@@ -10,9 +10,17 @@ class BasicSpider(scrapy.Spider):
         'http://quotes.toscrape.com/page/2/',
     ]
 
-    #执行爬取 scrapy crawl basic
+    # 执行爬取 scrapy crawl basic
+    # 提取并保存数据 scrapy crawl basic -o basic.json
     def parse(self, response):
         page = response.url.split("/")[-2]
         filename = 'toscrape-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
+        # 分析提取数据
+        for quote in response.css('div.quote'):
+            yield {
+                'title': quote.css('span.text::text').extract_first(),
+                'author': quote.css('small.author::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract(),
+            }
