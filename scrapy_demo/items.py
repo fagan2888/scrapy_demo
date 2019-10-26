b3021d1952152111561a67fccca6d759071658d2
kakuilan <kakuilan@163.com>
2019-10-26 15:07:20 +0800

    fix 声明item字段

--- scrapy_demo/items.py
+++ scrapy_demo/items.py
@@ -11,4 +11,20 @@ import scrapy
 class ScrapyDemoItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+
+    # Primary fields
+    title = scrapy.Field()
+    price = scrapy.Field()
+    description = scrapy.Field()
+    address = scrapy.Field()
+
+    # Calculate fields
+    images = scrapy.Field()
+    location = scrapy.Field()
+
+    # Housekeeping fields
+    url = scrapy.Field()
+    project = scrapy.Field()
+    spider = scrapy.Field()
+    server = scrapy.Field()
+    date = scrapy.Field()
